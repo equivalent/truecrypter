e61d1c0b58d54d47fec9ae439f2d16524f71ac02
Tomas Valent <equivalent@eq8.eu>
2012-09-20 11:04:00 +0100

    init Crypts

---
+++ paths_example.rb
@@ -0,0 +1,9 @@
+KEYFILE = "/home/my_user/Documents/my_key_file" # don't be a moron and store your keyfile some place safe
+MOUNT_FILE = "/home/my_user/Documents/my_truectrypt_file"
+
+
+# be sure MOUNT_FOLDER and BACKUP_FOLDER exist 
+# or create them e.g.: mkdir /home/my_user/Documents/mount_crypt_here/
+
+MOUNT_FOLDER = "/home/my_user/Documents/mount_crypt_here/"
+BACKUP_FOLDER = "/home/my_user/Documents/BACKUPS/"
